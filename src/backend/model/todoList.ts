002ccd5d87dea4da4824e3a281c7b6af13efb15a
Yajima Soichi <bildeneins@gmail.com>
2022-08-18 11:42:05 +0900

    Add sqlite connection to TodoList model

--- src/backend/model/todoList.ts
+++ src/backend/model/todoList.ts
@@ -1,40 +1,74 @@
+import sqlite3 from 'sqlite3';
 import { Todo } from "../../@types/common";
 
-// TODO: Use db to generate unique id
-let idCounter = 0;
-const getId = () => {
-  idCounter++;
-  return idCounter;
-};
 
 export default class TodoList {
   private _todos: Todo[];
+  private _db: sqlite3.Database;
 
-  constructor(todos?: Todo[]) {
-    if (todos != null) {
-      this._todos = todos;
-    } else {
-      this._todos = [];
-    }
+  private constructor() {
+    this._todos = [];
+    // an empty string for an anonymous disk-based database
+    this._db = new sqlite3.Database('');
+  }
+  
+  // set up new instance and return it
+  // asynchronous for database initialization
+  public static build(): Promise<TodoList> {
+    return new Promise((resolve, reject) => {
+      const tl = new TodoList();
+
+      // at most one statement object can execute a query at a time.
+      tl._db.serialize(() => {
+        tl._db.run('CREATE TABLE todo(id INTEGER PRIMARY KEY, name TEXT);', (err) => {
+          if (err) throw err;
+        });
+        tl._db.each('SELECT * FROM todo;', (err, row) => {
+          if (err) throw err;
+          tl._todos.push({
+            id: row.id,
+            name: row.name
+          });
+        }, () => {
+          // resolve on complete
+          resolve(tl);
+        })
+      })
+    })
   }
 
   public getTodos(): Todo[] {
     return this._todos;
   }
 
-  public add(name: string): Todo {
-    const id = getId();
-    const newTodo = { id, name };
-    this._todos.push(newTodo);
-    return newTodo;
+  public add(name: string): Promise<Todo> {
+    return new Promise((resolve, reject) => {
+      this._db.get('INSERT INTO todo(name) VALUES(?) RETURNING id;', [name], (err, row) => {
+        if (err) reject(err);
+        if (!row || row.id == null) reject('Could not add todo.');
+
+        const id = row.id;
+        const newTodo = { id, name };
+        this._todos.push(newTodo);
+        resolve(newTodo);
+      })
+    })
   }
 
-  public delete(todoId: Todo["id"]): void {
-    const idx = this._todos.findIndex((v) => v.id === todoId);
-    if (idx === -1) {
-      throw Error('Specified todo does not found.');
-    } else {
-      this._todos.splice(idx, 1);
-    }
+  public delete(todoId: Todo["id"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this._db.get(`DELETE FROM todo WHERE id=? RETURNING id;`, [todoId], (err, row) => {
+        if (err) reject(err);
+        if (!row || row.id == null) reject('Could not delete todo.');
+
+        const idx = this._todos.findIndex((v) => v.id === todoId);
+        if (idx === -1) {
+          reject('Specified todo does not found.');
+        } else {
+          this._todos.splice(idx, 1);
+          resolve();
+        }
+      });
+    });
   }
 }
